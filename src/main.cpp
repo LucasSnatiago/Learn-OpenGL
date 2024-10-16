32278a20d3beb83c27d496a5175376e5049c101f
Lucas Santiago <lu.santi.oli@gmail.com>
2024-10-16 12:43:06 -0300

    feat: updating opengl from 3.3.0 to 4.6.0

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,14 @@ extern "C" {
 }
 
 // Vertex shader for the triangle
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = "#version 460 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource = "#version 460 core\n"
     "out vec4 FragColor;\n"
     "void main() {\n"
     "FragColor = vec4(1.0f, 0.5f, 0.4f, 1.0f);\n"
@@ -38,8 +38,8 @@ void checkShaderCompilationStatus(unsigned int shader) {
 
 int main(int argc, char **argv) {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     glfwSetErrorCallback([](int error, const char* description) {
